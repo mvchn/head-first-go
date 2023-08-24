44dac583e38f5b16cc6af4305abbeddec372fcb5
mvchn <movchan@gmail.com>
2023-08-24 22:46:28 +0300

    func

--- ch03/dimensions.go
+++ ch03/dimensions.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
+func paintNeed(width float64, height float64) {
+	area := width * height
+	fmt.Printf("%.2f liters needed\n", area/10.0)
+}
+
 func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	paintNeed(4.2, 3.0)
+	paintNeed(5.2, 3.5)
+	paintNeed(5.0, 3.3)
 }
