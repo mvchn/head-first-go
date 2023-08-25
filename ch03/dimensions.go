47acf67dd65b6e797766f29a2cb8931d65ea18ff
mvchn <movchan@gmail.com>
2023-08-25 10:17:13 +0300

    multiply return

--- ch03/dimensions.go
+++ ch03/dimensions.go
@@ -2,17 +2,24 @@ package main
 
 import "fmt"
 
-func paintNeed(width float64, height float64) float64 {
+func paintNeed(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
-	var amount, total float64
-	amount = paintNeed(4.2, 3.0)
+	var total float64
+	amount, err := paintNeed(4.2, 3)
+	fmt.Println(err)
 	fmt.Printf("%0.2f liters needed\n", amount)
 	total += amount
-	amount = paintNeed(5.2, 3.5)
+	amount, err = paintNeed(5.2, 3.5)
 	fmt.Printf("%0.2f liters needed\n", amount)
 	total += amount
 	fmt.Printf("Total: %0.2f liters\n", total)
